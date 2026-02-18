8cd630d23697fc6d21e19a96a102fef279df2225
Jérémy Boureux <jeremyboureux@gmail.com>
2026-02-18 16:54:51 +0100

    test(web): configure Vitest and add smoke test for home page

---
+++ apps/web/src/app/page.test.tsx
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Page from './page'
+
+test('Page component should render text', () => {
+  render(<Page />)
+  expect(screen.getByText(/To get started, edit the page\.tsx file\./i)).toBeDefined()
+})
